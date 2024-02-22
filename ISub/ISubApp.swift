916a25b3c017fe981a7cbbdf02c90b0fb5ac6d82
aleksejtymo <aleksej.tymoshenko@icloud.com>
2024-02-22 19:58:27 +0100

    Initial Commit

---
+++ ISub/ISubApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISubApp.swift
+//  ISub
+//
+//  Created by Aleksej Tymoshenko on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ISubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
